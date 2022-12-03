69c924d65c7139fcafb28f3dfcc560bf86d695ea
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-03 00:03:22 -0500

    finished coding for showQuestion and advance

--- assets/script.js
+++ assets/script.js
@@ -6,13 +6,26 @@ var cursor = 0;
 var showQuestion = function () {
    for (var question of questions) {
     console.log(question);
+    if (question.dataset.index != cursor) {
+        if (question.dataset.index != cursor){
+            question.style.display = "none";
+        } else {
+            question.style.display = "block";
+        }
+    }
    }
    };
 
-var advance = function () {
+var advance = function (event) {
+    var element = event.target;
+    if (element.matches(".question button")) {
     if (cursor < questions.lengh - 1) {
         cursor++;
     }
     showQuestion();
+    }
 };
+
+document.addEventListener("CLICK", advance);
+
 showQuestion();

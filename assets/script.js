7739919e818a1288a33c6686d1f9342277558a9c
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-05 04:01:57 -0500

    came back to begining

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,22 @@
-/*var questions = document.querySelectorAll(".question");
-var cursor = 0;
 
-
-
-var showQuestion = function () {
-   for (var question of questions) {
-    console.log(question);
-    if (question.dataset.index != cursor) {
-        if (question.dataset.index != cursor){
-            question.style.display = "none";
-        } else {
-            question.style.display = "block";
-        }
-    }
-   }
-   };
-
-var advance = function (event) {
-    var element = event.target;
-    if (element.matches(".question button")) {
-    if (cursor < questions.lengh - 1) {
-        cursor++;
-    }
-    showQuestion();
-    }
-};
-
-document.addEventListener("CLICK", advance);
-
-showQuestion();
-
-*/
 var questions = document.querySelectorAll(".question");
 for (var i = 0; i < questions.length; i++) {
     questions[i].setAttribute("style", "color: white");
 }
+var nextEl = document.querySelector("#next");
+var cursor = 0;
+
+var showQuestions = function () {
+    for (var i = 0; i < questions.length; i++) {
+       console.log(questions[i]);
+    }
+}
+
+var advance = function() {
+    if (cursor < questions.length - 1) {
+        cursor++;
+    }
+    showQuestions()
+}
+
+showQuestions();

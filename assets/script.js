4ad9a90fe75cc2948666c4c319edc3970171bb5b
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-05 18:59:36 -0500

    added questions and answers

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
+var nextEl = document.querySelector("#next");
+var index = 0;
+var buttonContainer = document.querySelector(".container")
 
 var questions = document.querySelectorAll(".question");
 for (var i = 0; i < questions.length; i++) {
     questions[i].setAttribute("style", "color: white");
 }
-var nextEl = document.querySelector("#next");
-var cursor = 0;
+
+
 
 var showQuestions = function () {
     for (var i = 0; i < questions.length; i++) {
@@ -13,10 +16,19 @@ var showQuestions = function () {
 }
 
 var advance = function() {
-    if (cursor < questions.length - 1) {
-        cursor++;
+    if (index < questions.length - 1) {
+        index++;
+        
     }
     showQuestions()
 }
 
+buttonContainer.addEventListener("click", function(event) {
+    var element = event.target;
+    if (target.matches("li")) {
+      
+    }
+  });
+
 showQuestions();
+

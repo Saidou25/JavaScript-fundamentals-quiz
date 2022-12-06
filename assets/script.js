7ef3c839c15decf286d85704176b3455bf97f28e
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2022-12-05 20:06:59 -0500

    added back the buttons

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,9 @@
-var nextEl = document.querySelector("#next");
+//var nextEl = document.querySelector("#next");
 var index = 0;
-var buttonContainer = document.querySelector(".container")
-
+//var buttonContainer = document.querySelector(".container")
 var questions = document.querySelectorAll(".question");
+
+
 for (var i = 0; i < questions.length; i++) {
     questions[i].setAttribute("style", "color: white");
 }
@@ -12,23 +13,28 @@ for (var i = 0; i < questions.length; i++) {
 var showQuestions = function () {
     for (var i = 0; i < questions.length; i++) {
        console.log(questions[i]);
-    }
+       if(questions.data.index != index) {
+        questions.style.showQuestions = "none";
+       }
+       }   
 }
 
-var advance = function() {
+var advance = function(event) {
+    var element = event.target;
+    if (element.matches(".question.button")) {
     if (index < questions.length - 1) {
         index++;
-        
     }
-    showQuestions()
+    }
+    showQuestions();
 }
 
-buttonContainer.addEventListener("click", function(event) {
-    var element = event.target;
-    if (target.matches("li")) {
+document.addEventListener("click", advance) 
+   // var element = event.target;
+  //  if (target.matches("li")) {
       
-    }
-  });
+    
+ // });
 
 showQuestions();
 
